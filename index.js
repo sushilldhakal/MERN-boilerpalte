1dfa34cf3a8ec9af66e32f6b9525f6a1e96ef52c
sushill <sus.hill.dhakal@gmail.com>
2020-07-26 22:12:42 +1000

    move private info

--- index.js
+++ index.js
@@ -2,16 +2,36 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/key");
+
+const { User } = require("./models/user");
+
 mongoose
-  .connect(
-    "mongodb+srv://admin:sushill@testtravel-xcy06.gcp.mongodb.net/test?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.error(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ sucess: false, err });
+    res.status(200).json({
+      sucess: true,
+      userDate: doc,
+    });
+  });
 });
 
 app.listen(5000);
